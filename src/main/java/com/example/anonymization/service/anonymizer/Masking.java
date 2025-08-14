7d38acd17334878f23491e163658722cf6e125f6
paul2803-byte <feichtenschlager10@icloud.com>
2025-08-14 18:43:04 +0200

    Implementation of Masking

--- src/main/java/com/example/anonymization/service/anonymizer/Masking.java
+++ src/main/java/com/example/anonymization/service/anonymizer/Masking.java
@@ -1,5 +1,6 @@
 package com.example.anonymization.service.anonymizer;
 
+import com.example.anonymization.service.OntologyService;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Property;
@@ -10,6 +11,9 @@ import java.util.Map;
 public class Masking implements Anonymization {
     @Override
     public void applyAnoynmization(Model model, Property property, Map<Resource, Literal> data) {
-
+        Property anonymizedValue = model.createProperty(OntologyService.SOYA_URL, property.getLocalName() + "Anoynmized");
+        data.forEach((key, value) -> {
+            key.addLiteral(anonymizedValue, "*****");
+        });
     }
 }
